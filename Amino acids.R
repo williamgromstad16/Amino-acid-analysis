b2d47af2db9d0b1a60e9ccba12019a6f199196b2
williamgromstad16 <william.esbjug.gromstad@nmbu.no>
2024-11-08 15:34:18 +0100

    New script for future amino acid analysis

---
+++ Amino acids.R
@@ -0,0 +1,10 @@
+library(usethis)
+use_git_config(
+  user.name = "williamgromstad16", 
+  user.email = "william.esbjug.gromstad@nmbu.no"
+)
+
+# usethis::use_git()
+# use_github()
+
+# This is where I will do the future stable isotope analysis on amno acids.
